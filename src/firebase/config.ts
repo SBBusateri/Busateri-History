56990087bec511c59d75444c6491f2ca927fedc1
Blake Busateri <blakebusateri@MacBookAir.lan>
2025-10-06 14:27:42 -0500

    updated form

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBWtAi-Ti7wAwizTcb1h1m5olFysWFotY8",
   authDomain: "busateri-family-history.firebaseapp.com",
@@ -13,9 +8,9 @@ const firebaseConfig = {
   storageBucket: "busateri-family-history.firebasestorage.app",
   messagingSenderId: "801350935251",
   appId: "1:801350935251:web:0c51776cadd0bae5daea26",
-  measurementId: "G-JYDZF8FXXD"
+  measurementId: "G-JYDZF8FXXD",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
